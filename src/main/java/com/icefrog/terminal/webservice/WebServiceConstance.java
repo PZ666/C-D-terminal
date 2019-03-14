13cdeeb200c4dadb649fedd975ed952ebaa50c0d
icefrog <icefrog.su@qq.com>
2019-03-14 10:42:36 +0800

    移除无用常量

--- src/main/java/com/icefrog/terminal/webservice/WebServiceConstance.java
+++ src/main/java/com/icefrog/terminal/webservice/WebServiceConstance.java
@@ -26,7 +26,4 @@ public class WebServiceConstance {
 
 	// WSDL Address
 	public static final String WSDL = "http://211.88.20.132:8040/services/syncServiceStation?wsdl";
-	
-	// 公司编码
-	public static final String UserId = "4310270001";
 }
